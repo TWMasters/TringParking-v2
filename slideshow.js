6c18ac8caf176c93bdcf6f96c68c5c81336f3955
Tobias Masters <tobymasters10@gmail.com>
2023-01-11 18:28:56 +0000

    Add border to list and reposition text

--- slideshow.js
+++ slideshow.js
@@ -13,5 +13,5 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 5000); // Change image every 5 seconds
+  setTimeout(carousel, 10000); // Change image every 10 seconds
 }
