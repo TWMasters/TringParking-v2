706a53f7d7a0c7a31393e6eb35a1986ebd87ff91
Tobias Masters <tobymasters10@gmail.com>
2023-01-20 16:48:33 +0000

    JS to  open window

--- slideshow.js
+++ slideshow.js
@@ -2,6 +2,9 @@ var slideIndex = 0;
 
 window.onload = function () {
   carousel();
+  document.getElementById('terms-link').onclick = function() {
+    legalClick('terms.html');
+  }
 }
 
 function carousel() {
@@ -14,4 +17,12 @@ function carousel() {
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 5000); // Change image every 10 seconds
+}
+
+
+function legalClick(url) {
+  console.log("Legal Link Clicked");
+  var left = (screen.width/2)-(400);
+  var top = (screen.height / 10);
+  window.open(url, '_blank', 'height=1000,width=800,left='+left+',top='+top);
 }
