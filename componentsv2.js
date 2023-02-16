eb3d0bf6ff6db81ab680e9e57df7a0c4199e89b1
Tobias Masters <tobymasters10@gmail.com>
2023-02-16 17:15:52 +0000

    Refactor footer

--- componentsv2.js
+++ componentsv2.js
@@ -3,6 +3,12 @@ function loadHeaderFooter() {
   defineFooter();
 }
 
+function legalClick(url) {
+  var left = (screen.width/2)-(400);
+  var top = (screen.height / 10);
+  window.open(url, '_blank', 'height=1000,width=800,left='+left+',top='+top);
+}
+
 function defineHeader() {
   const tringHeader = document.getElementById('tring-header');
   tringHeader.innerHTML =
@@ -18,7 +24,20 @@ function defineHeader() {
 }
 
 function defineFooter() {
-
+  const tringHeader = document.getElementById('tring-footer');
+  tringHeader.innerHTML =
+  '<a id="terms-link">Terms & Conditions</a>' +
+  '<a id="privacy-link">Privacy Policy</a>' +
+  '<a id="legal-link">Legal Information</a>';
+  document.getElementById('terms-link').onclick = function() {
+    legalClick('terms.html');
+  }
+  document.getElementById('privacy-link').onclick = function() {
+    legalClick('privacy.html');
+  }
+  document.getElementById('legal-link').onclick = function() {
+    legalClick('legal.html');
+  }
 }
 
 /*Run as default if NO other scripts!*/
