6171a45a797ee92e73e4b4a1de8b8bca4fc14392
Tobias Masters <tobymasters10@gmail.com>
2023-02-22 14:40:18 +0000

    Create location page

--- scripts/componentsv2.js
+++ scripts/componentsv2.js
@@ -19,7 +19,7 @@ function defineHeader() {
     '<h1>PARKING AT TRING STATION</h1>' +
   '</div>' +  
   '<nav>' +
-    '<a class="page-link">Location Map</a>' +
+    '<a class="page-link" href="location.html">Location Map</a>' +
     '<a class="page-link" href="parkingbay.html">Parking Bay Map</a>' +
   '</nav>';
 }
